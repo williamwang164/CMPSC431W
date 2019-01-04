75d17e9571fd3d598bb63051fa46c3952a0f287d
William Wang <39848055+williamwang164@users.noreply.github.com>
2019-01-04 13:03:10 -0500

    Final Update

--- Demo/createDB.py
+++ Demo/createDB.py
@@ -2,9 +2,12 @@ import sqlite3 as sqlite
 import pandas as pd
 
 dataFile = pd.read_csv('Students.csv')
+# dataFile.columns = dataFile.columns.str.rstrip()
 colNames = list(dataFile.columns.values)
 print(type(colNames))
 
 conn = sqlite.connect('database.db')
 conn.text_factory = str
 dataFile.to_sql("Students", conn, index = False, if_exists='replace')
+cursor = conn.cursor()
+print(cursor.execute('PRAGMA TABLE_INFO(Students)').fetchall())
--- Demo/editDB.py
+++ Demo/editDB.py
@@ -4,13 +4,21 @@ import pandas as pd
 import sqlite3 as sqlite
 import createDB
 app = Flask(__name__)
+host = 'http://127.0.0.1:5000/'
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     dbConnection = sqlite.connect('database.db')
     cursor = dbConnection.cursor()
-    cursor.execute('SELECT * FROM Students ORDER BY Id ASC;')
-    result = cursor.fetchall()
-    return render_template('table.html', headers= createDB.colNames, data=result)
+    if request.method == 'POST':
+        sort = request.form['sortBy']
+        print(sort)
+        cursor.execute('SELECT * FROM Students ORDER BY "' + sort + '" ASC;')
+        result = cursor.fetchall()
+        return render_template('edit.html', headers= createDB.colNames, data=result)
+    return render_template('edit.html')
+
+
+
 if __name__ == '__main__':
    app.run(debug = True)
